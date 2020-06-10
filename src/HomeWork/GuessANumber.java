73c4d2a5b13644170606a8a03b6e4c03e2a89a3b
alekseyserdyukov <serdyukov.aleksey@gmail.com>
2020-06-10 13:40:25 +0300

    GuessANumber - added ranges

--- src/HomeWork/GuessANumber.java
+++ src/HomeWork/GuessANumber.java
@@ -4,29 +4,39 @@ import java.util.Scanner;
 
 public class GuessANumber {
     public static void startGame () {
+
         int inputedNumber;
         int trysCount = 5;
         int unknownNumber = Randomizer.getRandomNumWithRange(0,100);
+        int oldRange = 50;
+        int newRange;
+
         Scanner input = new Scanner(System.in);
         System.out.println("Привет! я загадал число от 0 до 100. У тебя есть 5 попыток его отгадать");
+
         do {
             System.out.print("Осталось " + trysCount + " попыток. Введи свое число:");
-
             inputedNumber = input.nextInt();
             trysCount--;
-            if (inputedNumber > unknownNumber)
-                System.out.println("Моё число меньше");
+            newRange = Math.abs(unknownNumber-inputedNumber);
 
-            else if (inputedNumber < unknownNumber)
-                System.out.println("Моё число больше");
-
-            else
+            if(inputedNumber==unknownNumber){
                 System.out.println("Ты угадал! Это число = " + unknownNumber);
+            }
+            else if (newRange > oldRange) {
+                System.out.println("Холоднее");
+            }
+            else if (newRange < oldRange) {
+                System.out.println("Теплее");
+            }
+
+            oldRange=newRange;
 
         } while (inputedNumber != unknownNumber && trysCount>0);
 
         if (trysCount == 0){
             System.out.println("Попытки исчерпаны, ты не угадал! Было загадано число = " + unknownNumber);
         }
+        System.exit(0);
     }
 }
